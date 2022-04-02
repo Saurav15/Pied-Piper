3f902f4e3027f4435576985b026d83865a9e684e
Dharmil Vakani <dharmil.v@simformsolutions.com>
2022-04-03 04:05:34 +0530

    added projectController and routes

---
+++ backend/src/models/projectModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+
+const projectSchema = mongoose.Schema({
+    projectManager:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Auth",
+        required:true
+    },
+    projectName:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    tentativeStartingDate:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    duration:{
+        type:Number,
+        required:true
+    },
+    selectedCandidates:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Developer"
+        },
+        
+    ],
+})
+
+const Project = mongoose.model("Project",projectSchema)
+module.exports = Project;
--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ require("./config/dbConfig");
 const cookieParser = require("cookie-parser");
 const loginRoute = require("./routes/loginRoute");
 const developerRoute = require("./routes/developerRoute");
+const projectRoute = require('./routes/projectRoute')
 const cors = require("cors");
 
 require("./utils/email/email");
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api", loginRoute);
 app.use("/api", developerRoute);
+app.use('/api',projectRoute)
 
 const port = process.env.PORT;
 app.listen(port, () => {
