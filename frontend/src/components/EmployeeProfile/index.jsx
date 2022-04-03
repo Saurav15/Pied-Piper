ddff26f1e1c84c8a17afa465f75269b0354bd0e2
JanhviHarwani <harwanijanhvi2000@gmail.com>
2022-04-03 07:30:47 +0530

    Employee-profile and Manager-profile

--- frontend/src/components/EmployeeProfile/index.jsx
+++ frontend/src/components/EmployeeProfile/index.jsx
@@ -1,13 +1,26 @@
-
-import React from 'react'
-import ProfileCard from '../UI/ProfileCard'
-
+import React from "react";
+import ProfileCard from "../UI/ProfileCard";
+import './Employee.css'
+import Avatar from "../../assests/images/483361.png";
 function EmployeeProfile() {
   return (
-   <ProfileCard>
-       
-   </ProfileCard>
-  )
+    <ProfileCard>
+      <img className="custom-margin" height={60} width={60} src={Avatar} alt="No preview available" />
+      <h5 className="custom-margin">Name</h5>
+      <h6 className="custom-margin">E-mail</h6>
+      <div className="custom-margin card d-flex flex-wrap w-200">
+        <div>
+          Skills:
+          <div className="d-flex flex-wrap">
+            <div className="p-2 card m-2">skill-1</div>
+            <div className="p-2 card m-2">skill-2</div>
+            <div className="p-2 card m-2">skill-3</div>
+            <div className="p-2 card m-2">skill-4</div>
+          </div>
+        </div>
+      </div>
+    </ProfileCard>
+  );
 }
 
-export default EmployeeProfile
+export default EmployeeProfile;
--- frontend/src/components/TeamleaderProfile/index.jsx
+++ frontend/src/components/TeamleaderProfile/index.jsx
@@ -1,8 +1,49 @@
 import React from "react";
 import ProfileCard from "../UI/ProfileCard";
-
+import Avatar from "../../assests/images/483361.png";
+import "./TeamLeader.css";
 function TeamLeaderProfile() {
-  return <ProfileCard></ProfileCard>;
+  return (
+    <ProfileCard>
+      <img className="custom-margin" height={60} width={60} src={Avatar} alt="No preview available" />
+      <h5 className="custom-margin">Name</h5>
+      <h6 className="custom-margin">E-mail</h6>
+      <div className="custom-margin card d-flex flex-wrap w-200">
+        <div>
+          Projects
+          <div className="d-flex flex-wrap">
+            <div className="p-2 card m-2">
+              Previous Projects:
+              <div>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi
+                enim cupiditate sapiente sint officiis vel voluptatem tempora
+                dolores eius provident officia debitis saepe sed, nobis
+                aspernatur reprehenderit illum pariatur ipsam.
+              </div>
+            </div>
+            <div className="p-2 card m-2">
+              Ongoing Projects:
+              <div>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi
+                enim cupiditate sapiente sint officiis vel voluptatem tempora
+                dolores eius provident officia debitis saepe sed, nobis
+                aspernatur reprehenderit illum pariatur ipsam.
+              </div>
+            </div>
+            <div className="p-2 card m-2">
+              Upcoming Projects:
+              <div>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi
+                enim cupiditate sapiente sint officiis vel voluptatem tempora
+                dolores eius provident officia debitis saepe sed, nobis
+                aspernatur reprehenderit illum pariatur ipsam.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </ProfileCard>
+  );
 }
 
 export default TeamLeaderProfile;
